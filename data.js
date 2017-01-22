e37e58cead534ce36fc794253a12a0831f51a5e2
ozerone <ozerone@protonmail.com>
2017-01-22 16:05:40 -0700

    minified

--- data.js
+++ data.js
@@ -1,15 +1,17 @@
 var db;
-var title, body;
+var title, body, date;
 loadData = function() {
   console.log("ready");
-  $.getJSON("data.json", function(data) {
+  $.getJSON("data.min.json", function(data) {
       db = data;
       title = document.getElementById('main-title');
       body = document.getElementById('main-body');
+      date = document.getElementById('main-date');
     })
     .done(function() {
       title.innerHTML = db.articles[0].title;
       body.innerHTML = db.articles[0].body;
+      date.innerHTML = db.articles[0].date;
 
     });
 
